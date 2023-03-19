3c0ef31e95ac5ee5ea2f1aece1cc61012e1c269a
Owner <artemdem972@gmail.com>
2023-03-19 11:37:00 +0300

    v 1.0.4

--- SteamStorage/MainWindow/MainWindow.xaml.cs
+++ SteamStorage/MainWindow/MainWindow.xaml.cs
@@ -20,7 +20,14 @@ namespace SteamStorage
             RemainsPageInstance.Version.Text = Constants.Version;
             ArchivePageInstance.Version.Text = Constants.Version;
             MainFrame.Content = RemainsPageInstance;
-            MaxHeight = System.Windows.SystemParameters.PrimaryScreenHeight - 26;
+
+            double height = SystemParameters.PrimaryScreenHeight / 1.75;
+            double width = SystemParameters.PrimaryScreenWidth / 1.75;
+            Height = height > 630 ? 630 : height;
+            Width = width > 1120 ? 1120 : width;
+
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
         }
         private void ShowCloseMessage(object sender, CancelEventArgs e)
         {
